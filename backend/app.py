775553a3de40d24fe17f51e32a593d964299f3ad
Ramana1205 <gorlaramana05@gmail.com>
2026-08-15 20:03:02 +0530

    fixed cors error

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://YOUR-VERCEL-URL.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
